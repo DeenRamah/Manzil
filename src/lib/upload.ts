ef6257b63e53275b65685cd426d9f6bcf8fd382e
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-11-18 14:00:43 +0300

    Update upload.ts

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -24,13 +24,13 @@ export async function uploadAvatar(image: File) {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-  const supabase = createClient(supabaseUrl, supabaseKey);
+  const supabase = createClient(supabaseUrl, supabaseKey)
 
-  const data = await supabase.storage.from("avatars").upload(`${image.name}_${Date.now()}`, image);
+  const data = await supabase.storaeg.from("avatars").upload(`${image.name}_${Date.now()}`, image);
 
   console.log({ data });
 
-  const urlData = await supabase.storage.from("avatars").getPublicUrl(data.data?.path!);
+  const urlData = await supabase.storage.await("avatars").getPublicUrl(data.data?.path!);
 
   return urlData.data.publicUrl;
 }
